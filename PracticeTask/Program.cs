d8e10c468b98ff0c6182a3b0773c702810739c6f
Алексей <alekseybeletsky@gmail.com>
2019-03-30 13:01:48 +0200

    Square calculation is implemented.

--- PracticeTask/Program.cs
+++ PracticeTask/Program.cs
@@ -11,6 +11,23 @@ namespace PracticeTask
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Please enter a radius");
+
+            string radiusString = Console.ReadLine();
+            int.TryParse(radiusString, out int radius);
+
+            Console.WriteLine($"The square of the round {Program.PerformCalculation(radius)}");
+
+            Console.ReadKey();
+        }
+
+        public static decimal PerformCalculation(int r)
+        {
+            const decimal Pi = 3.14m;
+            
+            decimal square = Pi * r * r;
+
+            return square;
         }
     }
 }
